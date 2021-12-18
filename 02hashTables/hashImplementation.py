0b37d7f4cd461776703aa40f1a16bbb2317e6fd0
Rakesh Gunelly <rakeshgunelly@gmail.com>
2021-12-18 21:22:59 +0530

    hashmap set implementation

--- 02hashTables/hashImplementation.py
+++ 02hashTables/hashImplementation.py
@@ -1,8 +1,10 @@
 #Hash map implementation in Python
 
+
 class HashTable :
     
     def __init__(self,size) -> None:
+        self.size = size
         self.data = [[] for i in range(size)]
         
 
@@ -12,35 +14,33 @@ class HashTable :
         hash = 0
 
         for i in range(0,len(key)-1):
-            print(ord(key[i]))
             hash = (hash + ord(key[i])*i)%(len(self.data))   #ord() function return unicode of a given character
-        print("\n"+str(hash))
+        
         return hash
 
-
+    # returns the hash value using the private hash method
+    def get_hash_value(self,key):
+        return self._hash(key)
+    
     # For creating a [key,value] pair
     def set(self,key,value):       
-        pass
-        # address = self._hash(key)
-        # if self.data[address] !=[]:
-        #     self.data.insert(address,[key,value])
         
+        hash = self.get_hash_value(key)
+    
+        if self.data[hash]==[]:
+            self.data[hash]=[key,value]
+        elif self.data[hash][0]==key:
+            print("Given key is already present in the hashmap, please enter a different key")
+  
+
     #For fetching the value based on the given key
     def get(self,key):
-        hash = self._hash(key)
-        
-        #for i in range(len(self.data[hash])):
-        #    if self.data[i][0] == key:
-        #        return self.data[i][1]
+        hash = self.get_hash_value(key)
+        print(hash)
+        if self.data[hash][0]==key:
+            return self.data[hash][1]
 
-        reference = self.data[hash][0]
-        print(reference)
-    
-        #for i in range(len(self.data)):
-        #    if reference[i][0]==key:
-        #        return reference[i][1]
-            
-        #return "Given Key has no corresponding value pair"
+        return "Given Key has no corresponding value pair"
 
     
     # For removing a [key,value] pair in a hash map
@@ -54,13 +54,11 @@ class HashTable :
 
 some_hash = HashTable(50)
 some_hash.set('grapes',10000)
-# some_hash.set('grapes',10000)
-# some_hash.set('grapes',10000)
-some_hash.set('grapes',10000)
-#some_hash.set('apples',222)
-#some_hash.set('mango',111)
+some_hash.set('grape',10000)
+some_hash.set('apples',222)
+some_hash.set('mango',111)
 print(some_hash.data)                           # not a good way to access class data, using here only for verification
-#print(some_hash.get('grapes'))
-#print(some_hash.get('grap'))
+print(some_hash.get('grapes'))
+print(some_hash.get('grap'))
 #some_hash.remove('mango')
 #print(some_hash.data) 

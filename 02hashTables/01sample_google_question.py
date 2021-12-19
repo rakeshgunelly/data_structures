1785dc9486f064f1eed34e9b8ecc2669e739ca7e
Rakesh Gunelly <rakeshgunelly@gmail.com>
2021-12-19 19:38:57 +0530

    Solution for Google's first recurring item in an array

---
+++ 02hashTables/01sample_google_question.py
@@ -0,0 +1,42 @@
+# Google Question 
+# Given am array = [2,5,1,2,3,5,1,2,4]:
+# return the first reccuring item i.e 2
+
+# Given an array = [2,1,1,2,3,5,1,2,4]:
+# it should return 1
+
+# Given an array = [2,3,4,5]
+# it should return undefined
+
+# A brute force approch is to loop througth the given array and see if the given item is present in the array
+# it will be of O(n^2) time complexity
+
+# Brute force approach | Time complexity is O(n^2)
+
+def first_reccurring_item(input):
+
+    for i in range(len(input)-1):
+        for j in range(i+1,len(input)-1):
+            if input[i]==input[j]:
+                return input[i]
+    
+    return False
+
+
+
+
+# Using hashmaps:
+
+def first_reccurring_item2(input):
+    mydict = {}
+    for i in range(len(input)):
+        if input[i] in mydict:
+            return input[i]
+        else:
+            mydict[input[i]]=i
+
+    return 'No Reccuring values are present'
+
+
+
+print(first_reccurring_item2([2,3,4,5]))

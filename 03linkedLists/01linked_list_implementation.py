37c9897783df3a8eeb144b3b8e9a775591ca6a1d
Rakesh Gunelly <rakeshgunelly@gmail.com>
2021-12-23 19:46:38 +0530

    linked list append method

--- 03linkedLists/01linked_list_implementation.py
+++ 03linkedLists/01linked_list_implementation.py
@@ -13,3 +13,30 @@ class LinkedList():
     def __init__(self,head):
         self.head = None
         self.tail = None
+
+    def append(self,data):
+        new_node = Node(data)
+
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+            self.length = 1
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+            self.length += 1
+
+    def preappend(self,data):
+        pass
+
+    def insert(self,data):
+        pass
+
+    def delete(self,data):
+        pass
+
+    def print_linked_list(self,data):
+        pass
+
+    def reverse(self):
+        pass

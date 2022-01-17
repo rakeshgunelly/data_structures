da9a629497fbdcdbab03bf76ed1633284efb7d58
Rakesh Gunelly <rakeshgunelly@gmail.com>
2022-01-17 20:37:35 +0530

    BST

---
+++ 05binary_search_tree/01bst_implementation.py
@@ -0,0 +1,26 @@
+class Node():
+
+    def __init__(self) -> None:
+        self.left = None
+        self.right = None
+        self.value = None
+
+class BinarySearchTree():
+
+    def __init__(self) -> None:
+        self.root = None
+
+
+    def insert(self):
+        pass
+
+    def lookup(self):
+        pass
+
+
+#           9
+#         /   \
+#      4      20
+#     /   \   /   \
+#   1     6  15   170
+tree = BinarySearchTree()

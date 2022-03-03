832bf0eed020c13224d8f36e8378a74010efe13d
Rakesh Gunelly <rakeshgunelly@gmail.com>
2022-03-03 22:03:27 +0530

    Backspace String Compare using inbuilt functions

--- Leetcode_questions/54backspace_string_compare_leetcode_844.py
+++ Leetcode_questions/54backspace_string_compare_leetcode_844.py
@@ -44,3 +44,14 @@ class Solution:
         return build(s)==build(t)
             
             
+from functools import reduce
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        def back(res,c):
+            if c!="#":res.append(c)
+            elif res: res.pop()
+            return res
+        return reduce(back,s,[])==reduce(back,t,[])
+            
+            
+            
